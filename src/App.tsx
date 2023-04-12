7871735b39b88192e694678afc3f0bcdb0c046f0
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-04-13 01:16:47 +0600

    Navbar Link Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Navbar from "@/components/navbar";
+import { useState } from "react";
+
 function App() {
-  return <div className="app">app</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  return <div className="app">
+    <Navbar
+       selectedPage={selectedPage}
+       setSelectedPage={setSelectedPage}
+    />
+  </div>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-100" : "#8b3dff",
+      },
+      fontFamily: {
+        sans: ["Noto Sans", "sans-serif"],
+      },
+      screens: {
+        xs: "480px",
+        sm: "768px",
+        md: "1060px",
+      },
+    },
+  },
+  plugins: [],
+}
