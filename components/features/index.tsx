f8e1414f40c634522ec4e98bf548737fdf9a3a20
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-05-01 13:58:51 +0600

    Heading and description add

--- components/features/index.tsx
+++ components/features/index.tsx
@@ -37,39 +37,43 @@ const features: Array<FeatureType> = [
 const Features = (props: Props) => {
     const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)')
     const featureItems = features.map((feature, index) =>
-        <div
-            key={index}
-            className={"mt-16 items-center justify-between gap-20 md:mt-28 md:flex " + (index % 2 && "md:flex-row-reverse")}
-        >
-            {feature.IsVideoUrl ? (<video src={feature.MediaUrl} autoPlay loop muted className="md:w-1/2 mt-8"></video>) : (
-                <Image
-                width={500}
-                height={500}
-                src={feature.MediaUrl}
-                alt="feature image"
-                className="md:w-1/2 mt-8"
-                />
-            )}
-            <div className="ps-10">
-                <HText>{feature.Title}</HText>
-                <motion.div
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 0.5 }}
-                    variants={{
-                        hidden: { opacity: 0, x: 50 },
-                        visible: { opacity: 1, x: 0 },
-                    }}
-                >
-                    <p className="my-2">
-                        {feature.Description}
-                    </p>
-                </motion.div>
-                <div className="mt-10">
-                    <ActionButton setSelectedPage={props.setSelectedPage}>
-                        {feature.ButtonText}
-                    </ActionButton>
+        <div  className={"flex flex-col justify-center pt-48 text-center"}>
+            <h1 className='text-3xl font-bold text-gray-800 font-sand mb-2'>We are full of surprises</h1>
+            <p className='text-gray-600 text-lg mt-1'>Here are some of our best traits you might not know about</p>
+            <div
+                key={index}
+                className={"mt-24 items-center justify-between gap-20 md:flex " + (index % 2 && "md:flex-row-reverse")}
+            >
+                {feature.IsVideoUrl ? (<video src={feature.MediaUrl} autoPlay loop muted className="md:w-1/2 mt-8"></video>) : (
+                    <Image
+                        width={500}
+                        height={500}
+                        src={feature.MediaUrl}
+                        alt="feature image"
+                        className="md:w-1/2 mt-8"
+                    />
+                )}
+                <div className="ps-10">
+                    <HText>{feature.Title}</HText>
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: 50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                    >
+                        <p className="my-2">
+                            {feature.Description}
+                        </p>
+                    </motion.div>
+                    <div className="mt-10">
+                        <ActionButton setSelectedPage={props.setSelectedPage}>
+                            {feature.ButtonText}
+                        </ActionButton>
+                    </div>
                 </div>
             </div>
         </div>
