e34e1c71dd25400c1bdf289ee341dfda920eccdc
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-05-01 14:01:37 +0600

    Action button color add, tailwind not working

--- shared/ActionButton.tsx
+++ shared/ActionButton.tsx
@@ -11,7 +11,7 @@ const ActionButton = ({children, setSelectedPage}: Props) => {
   return (
     <Link
     className='rounded bg-primary-100 text-white py-2 px-4 focus:outline-none focus:shadow-outline'
-    style={{backgroundColor: '#8b3dff', color: '#fff', borderRadius: '4px', padding:'0.5 1rem'}}
+    style={{backgroundColor: '#8b3dff', color: '#fff', borderRadius: '4px', padding:'0.5rem 1rem'}}
     onClick={() => setSelectedPage(SelectedPage.Charu)}
     href={`${SelectedPage.Charu}`}
     >
