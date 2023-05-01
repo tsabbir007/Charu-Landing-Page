7f2e8d60afbf776ee74480cc0b084a2bb4a26f38
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-05-01 13:58:29 +0600

    Saas add

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains:["content-management-files.canva.com", "static.canva.com", "template.canva.com"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
