84b71c4aad9fdc9b78e69aa8e130fd843b3800ec
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-05-09 19:33:07 +0600

    single post page UI complete

--- pages/blog/[postSlug].tsx
+++ pages/blog/[postSlug].tsx
@@ -6,6 +6,11 @@ import { SelectedPage } from "@/shared/types";
 import { useState } from "react";
 import { useEffect } from "react";
 import Image from "next/image";
+import { Breadcrumbs, Link, Typography } from "@mui/material";
+import styles from "./post.module.scss";
+import "@wordpress/block-library/build-style/style.css";
+
+
 
 interface PostProps {
   featuredImage: {
@@ -75,7 +80,7 @@ export default function Post({ postData }: { postData: any }) {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <div>
+    <div className={`${styles.single_post} font-sand`}>
       <Head>
         <title key={postData.slug}>{postData.title}</title>
         <meta name="description" content={postData.excerpt} key="metadescription" />
@@ -86,16 +91,41 @@ export default function Post({ postData }: { postData: any }) {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <div className="container mt-32 mx-auto md:px-36">
-        <Image
-          width={postData.featuredImage.node.mediaDetails.sizes[1].width}
-          height={postData.featuredImage.node.mediaDetails.sizes[1].height}
-          src={postData.featuredImage.node.mediaDetails.sizes[1].sourceUrl}
-          alt="slider"
-          className="rounded-lg w-full h-auto"
-        />
-        <h1 className="text-4xl font-bold">{postData.title}</h1>
-        <div className="text-lg mt-4" dangerouslySetInnerHTML={{ __html: postData.content }} />
+      <div className="container-fluid mt-36 md:my-32 mx-auto md:px-36">
+        <Breadcrumbs separator="›" aria-label="breadcrumb" className="ms-8 md:ms-0 mb-3">
+          <Link underline="hover" color="black" href="/" className="text-normal text-lg">
+            Blog
+          </Link>
+          <Typography color="text.primary-500" className="text-normal md:text-lg">{postData.title}</Typography>
+        </Breadcrumbs>
+      </div>
+      <div className={`${styles.top_banner}`}>
+        <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 md:px-[12rem]`}>
+          <div className="p-4">
+            <h1 className="text-4xl font-bold font-sand leading-snug">{postData.title}</h1>
+            {/* <p className="text-lg mt-5" dangerouslySetInnerHTML={{ __html: postData.excerpt }}></p> */}
+            <p className="text-xl mt-10">Learn how to use design thinking to solve your team’s most challenging problems.</p>
+            <div className="my-10">
+              <Link
+                className='rounded bg-primary-100 text-white py-2 px-4 focus:outline-none focus:shadow-outline'
+                style={{ backgroundColor: '#8b3dff', color: '#fff', borderRadius: '4px', padding: '0.5rem 1rem', fontWeight: 900, fontSize: '1.2rem' }}
+                href="#features"
+              >
+                Start your free trial
+              </Link>
+            </div>
+          </div>
+          <Image
+            width={postData.featuredImage.node.mediaDetails.sizes[1].width}
+            height={postData.featuredImage.node.mediaDetails.sizes[1].height}
+            src={postData.featuredImage.node.mediaDetails.sizes[1].sourceUrl}
+            alt="slider"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
+      <div className="container mt-20 mx-auto md:px-36">
+        <div className={`${styles.content} text-[22px] tracking-normal mt-4 leading-snug px-4 md:px-0`} dangerouslySetInnerHTML={{ __html: postData.content }} />
       </div>
       <Footer setSelectedPage={setSelectedPage} />
     </div>
